e0728a6675f2007bb8d76d66d59f9764a54bc8f5
Nathan Mena <natemena153@gmail.com>
2022-02-11 10:18:53 -0700

    Init commit

---
+++ sldc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and 
+    set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+
+const analysisOfRequirements = "This is the second step. This step is where you begin to look into what technologies are required for the application, or what is needed with staffing or even third parties to power the application and technologies."
+
+const design = "This is the third step. This is when business establishes a document that paints an overall picture of how the application is going to be designed and how components are going to be tied together. This may even involve some PoC's or prototypes."
+
+const implementation = "This is the fourth step. This is where the code development of the application truly begins. This is the part of the process where you begin to develop a full product based off the design."
+
+const testingAndIntegration = "This is the fifth step. This is when testing is done on builds and work to polish a product and make it the best as it can and aim to have it as defect free for when the application is released."
+
+const maintain = "This is the final step. This is when the application or product has been released and you need to maintain it because ineviteably there are flaws in the product or problems that may arise from the technologies implemented such as servers that need to be addressed. This may include security patches, deploying more servers, patching the applicaiton to address security vulnerabilities, etc."
